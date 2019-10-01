fd26802fe4d4f5d54089c852db24613d17b07c6f
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2019-10-01 03:30:51 -0300

    Update Script

--- Install/ger-tools/htop.sh
+++ Install/ger-tools/htop.sh
@@ -30,6 +30,7 @@ sleep 1s
 echo -e "${cor[3]} $(fun_trans "Con htop puedes ver todos los procesos")"
 echo -e "${cor[3]} $(fun_trans "que se ejecutan en tu sistema")"
 echo -e "${cor[3]} $(fun_trans "PARA SALIR DEL PANEL PRESIONE") ${cor[3]}CTLR+C"
+echo -e "$barra"
 fun_bar "apt-get install htop -y"
 sleep 1s
 
