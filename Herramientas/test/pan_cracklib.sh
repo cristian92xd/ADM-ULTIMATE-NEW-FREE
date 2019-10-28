4caaa2af510f393566fe8c1c635c54c2eca9500e
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2019-10-28 19:37:58 -0300

    Update Script

--- Herramientas/test/pan_cracklib.sh
+++ Herramientas/test/pan_cracklib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;35m" [3]="\033[1;32m" [4]="\033[1;31m" [5]="\033[1;33m" [6]="\E[44;1;37m" [7]="\E[41;1;37m" )
 barra="\033[0m\e[31m======================================================\033[1;37m"
+
 fun_bar () {
 comando="$1"
  _=$(
@@ -21,6 +22,7 @@ done
 echo -e " \033[1;33m[\033[1;31m####################\033[1;33m] - \033[1;32m100%\033[0m"
 sleep 1s
 }
+
 pamcrack () {
 echo -e "${cor[5]}LIBERANDO PASSWD PARA VURTL"
 sed -i 's/.*pam_cracklib.so.*/password sufficient pam_unix.so sha512 shadow nullok try_first_pass #use_authtok/' /etc/pam.d/common-password
@@ -28,7 +30,7 @@ echo -e "${cor[5]}LISTO YA PUEDES CREAR USUARIOS"
 }
 
 shadow_fun () {
-echo -e " ${cor[7]}PERMISO ROOT VURTL ${cor[6]}[ALEX_DROID9_0_MX]\033[0m"
+echo -e " ${cor[7]}PERMISO ROOT VURTL ${cor[6]}[NEWADM]\033[0m"
 echo -e "$barra"
 while true; do
 echo -e "${cor[4]} [1] > ${cor[5]}DESBLOQUEAR VURTL PARA CREAR USUARIOS"
