2830fbacd93b4390fb65cdc4dd309abd6adecd92
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2020-05-20 13:47:22 -0400

    Update Script

--- request/speed.sh
+++ request/speed.sh
@@ -1,61 +1,32 @@
 #!/bin/bash
 declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;31m" [3]="\033[1;33m" [4]="\033[1;32m" )
+SCPdir="/etc/newadm" && [[ ! -d ${SCPdir} ]] && exit 1
+SCPfrm="/etc/ger-frm" && [[ ! -d ${SCPfrm} ]] && exit
+SCPinst="/etc/ger-inst" && [[ ! -d ${SCPinst} ]] && exit
+SCPidioma="${SCPdir}/idioma" && [[ ! -e ${SCPidioma} ]] && touch ${SCPidioma}
 
-link_bin="https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/HERRAMIENTAS/master/Testador-Velocidad/speedtest"
-[[ ! -e /bin/speedtest ]] && wget -O /bin/speedtest ${link_bin} > /dev/null 2>&1 && chmod +x /bin/speedtest
-
-apt-get install python3 -y > /dev/null 2>&1
-apt-get install python-pip -y > /dev/null 2>&1
-pip install speedtest-cli > /dev/null 2>&1
-
-velocity () {
-aguarde () {
-comando[0]="$1"
-comando[1]="$2"
- (
-[[ -e $HOME/fim ]] && rm $HOME/fim
-${comando[0]} > /dev/null 2>&1
-${comando[1]} > /dev/null 2>&1
-touch $HOME/fim
- ) > /dev/null 2>&1 &
- tput civis
-echo -ne "  \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
-while true; do
-   for((i=0; i<18; i++)); do
-   echo -ne "\033[1;31m#"
-   sleep 0.1s
-   done
-   [[ -e $HOME/fim ]] && rm $HOME/fim && break
-   echo -e "\033[1;33m]"
-   sleep 1s
-   tput cuu1
-   tput dl1
-   echo -ne "  \033[1;33mAGUARDE \033[1;37m- \033[1;33m["
+echo -e "${cor[3]} $(fun_trans "Speed Test") ${cor[4]}[NEW-ADM]"
+msg -bar
+ping=$(ping -c1 google.com |awk '{print $8 $9}' |grep -v loss |cut -d = -f2 |sed ':a;N;s/\n//g;ta')
+# PROGRESS - BAR
+(
+echo -ne "[" >&2
+while [[ ! -e /tmp/pyend ]]; do
+echo -ne "." >&2
+sleep 0.8s
 done
-echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-tput cnorm
-}
-
-fun_tst () {
-speedtest --share > speed
-}
-
-echo -e " \033[1;33mSpeed Test \033[1;32m[NEW-ADM] \033[0m"
-echo -e "\033[0;34m======================================================\033[0m"
-aguarde 'fun_tst'
-png=$(cat speed | sed -n '5 p' |awk -F : {'print $NF'})
-down=$(cat speed | sed -n '7 p' |awk -F :  {'print $NF'})
-upl=$(cat speed | sed -n '9 p' |awk -F :  {'print $NF'})
-lnk=$(cat speed | sed -n '10 p' |awk {'print $NF'})
-echo -e "\033[0;34m======================================================\033[0m"
-echo -e "\033[1;32mLatencia:\033[1;37m$png"
-echo -e "\033[1;32mDownload:\033[1;37m$down"
-echo -e "\033[1;32mUpload:\033[1;37m$upl"
-echo -e "\033[1;32mResult: \033[1;36m$lnk\033[0m"
-echo -e "\033[0;34m======================================================\033[0m"
-rm -rf $HOME/speed
-}
-
-velocity
-
-#fim
+rm /tmp/pyend
+echo -e "]" >&2
+) &
+[[ $(dpkg --get-selections|grep -w "python"|head -1) ]] || apt-get install python -y &>/dev/null
+starts_test=$(python ${SCPfrm}/speedtest.py --share) && touch /tmp/pyend
+sleep 0.6s
+tput cuu1 && tput dl1
+up_load=$(echo "$starts_test" | grep "Upload" | awk '{print $2,$3}')
+down_load=$(echo "$starts_test" | grep "Download" | awk '{print $2,$3}')
+re_sult=$(echo "$starts_test" | grep "result" | awk '{print $3}')
+msg -ama " \033[1;32m$(fun_trans "Latencia"): \033[1;37m$ping"
+msg -ama " \033[1;32m$(fun_trans "Upload"): \033[1;37m$up_load"
+msg -ama " \033[1;32m$(fun_trans "Download"): \033[1;37m$down_load"
+msg -ama " \033[1;32m$(fun_trans "Result"): \033[1;33m$re_sult"
+msg -bar 
